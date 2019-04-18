37dc406bfe0128f3f48254c94191958974f48aaf
biggabisanta <gaah.dedelly@gmail.com>
2019-04-17 22:20:41 -0300

    exercicio2

--- 02/Ex01-news/js/script.js
+++ 02/Ex01-news/js/script.js
@@ -1,9 +1,18 @@
-var mensagem = document.querySelector('.mensagem');
+ var mensagem = document.querySelector('.mensagem');
 
 
 var botao = document.querySelector('.button');
-botao.addEventListener('click',function(evento){
+botao.addEventListener('click', function (evento) {
     evento.preventDefault();
     var email = document.querySelector('#newsInputEmail')
-       mensagem.textContent = 'O email '+ email.value +' foi cadastrado com sucesso!'
-})
+    mensagem.textContent = 'O email ' + email.value + ' foi cadastrado com sucesso!'
+}) 
+
+var email = 0
+if (email.value == "" || email.value.length == 0){
+    mensagem.textContent = 'Digite um email'
+} else if (email.value.indexOf('@')>=0 && email.value.indexOf('@')<=email.value.length-1) {
+     mensagem.textContent = 'O email informado é inválido'
+    } else {
+         mensagem.textContent = 'O email' + email.value + ' foi cadastrado com sucesso'
+}
